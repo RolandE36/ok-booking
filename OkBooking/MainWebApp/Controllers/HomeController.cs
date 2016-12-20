19c6f5c8d2277f1263fc6b6cb7cabc25c2dbaef8
Roman Lytvyn <Roman.Lytvyn@verint.com>
2016-12-20 17:03:44 +0200

    Created login page. UI only.

---
+++ OkBooking/MainWebApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainWebApp.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			// https://dcrazed.com/css-html-login-form-templates/ - Login design
+			return View();
+		}
+	}
+}
---
+++ OkBooking/MainWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MainWebApp.Startup))]
+namespace MainWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
