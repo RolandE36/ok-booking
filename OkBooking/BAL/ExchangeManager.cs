165083a0afc010b8d8b84741793bb4314907e735
Victoria Gerasymchuk <victoria.gerasymchuk@gmail.com>
2017-01-09 21:06:54 +0200

    Fixed time zone on UI.

--- OkBooking/BAL/ExchangeManager.cs
+++ OkBooking/BAL/ExchangeManager.cs
@@ -58,29 +58,34 @@ namespace BAL {
 
 				// go through each meeting in the room and check room availability
 				if (item != null && item.CalendarEvents.Count > 0) {
-					foreach (var cevent in item.CalendarEvents) {
+					foreach (var meeting in item.CalendarEvents) {
+
+						// set time zone
+						DateTime meetingStartTime = TimeZoneInfo.ConvertTime(meeting.StartTime, service.TimeZone);
+						DateTime meetingEndTime = TimeZoneInfo.ConvertTime(meeting.EndTime, service.TimeZone);
+
 						// skip finished meeting
-						if (cevent.StartTime < userTimeNow && cevent.EndTime < userTimeNow) continue;
+						if (meeting.StartTime < userTimeNow && meeting.EndTime < userTimeNow) continue;
 
 						// meeting in progress
-						if (cevent.StartTime <= userTimeNow && cevent.EndTime >= userTimeNow) {
-							message += "... - " + cevent.EndTime.ToString("HH: mm") + "; ";
+						if (meeting.StartTime <= userTimeNow && meeting.EndTime >= userTimeNow) {
+							message += "... - " + meetingEndTime.ToString("HH:mm") + "; ";
 							bookNow = false;
 						}
 
 						// future meeting
-						if (cevent.StartTime > userTimeNow && cevent.EndTime >= userTimeNow) {
+						if (meeting.StartTime > userTimeNow && meeting.EndTime >= userTimeNow) {
 
 							// we can't book room if we have only 15 minutes till the next meeting
-							var span = new TimeSpan(cevent.StartTime.Ticks - userTimeNow.Ticks);
+							var span = new TimeSpan(meeting.StartTime.Ticks - userTimeNow.Ticks);
 							if (bookNow && span.Minutes <= 15) { bookNow = false; }
 
-							message = string.Format("{0} - {1}", cevent.StartTime.ToString("HH: mm"), cevent.EndTime.ToString("HH: mm"));
+							message = string.Format("{0} - {1}", meetingStartTime.ToString("HH:mm"), meetingEndTime.ToString("HH:mm"));
 							break;
 						}
 
 						// skip next days
-						if (cevent.StartTime.Day != userTimeNow.Day) break;
+						if (meeting.StartTime.Day != userTimeNow.Day) break;
 					}
 				}
 
