b920e9fcce76b99c63a7894fe262c9966f9fda1e
Roman Lytvyn <Roman.Lytvyn@verint.com>
2017-01-26 18:27:20 +0200

    Updated MySql DB connection string.

--- OkBooking/DAL/Context.cs
+++ OkBooking/DAL/Context.cs
@@ -13,7 +13,7 @@ namespace DAL
 #if DEBUG
 		public Context() : base() {}
 #else
-		public Context() : base(Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb", EnvironmentVariableTarget.Process)) {
+		public Context() : base(Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb")) {
 			//Configuration.LazyLoadingEnabled = true;
 		}
 #endif
