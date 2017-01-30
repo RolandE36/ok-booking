53fc996df16d50a32812e8d379aabbe8ffca3f8a
Roman Lytvyn <Roman.Lytvyn@verint.com>
2017-01-30 18:22:12 +0200

    Updated MySQL connection settings.

--- OkBooking/DAL/Context.cs
+++ OkBooking/DAL/Context.cs
@@ -8,8 +8,21 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
+#if (!DEBUG)
+	[DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+#endif
 	public class Context : DbContext {
-		public Context() : base() {}
+#if DEBUG
+		public Context() : base("DebugConnection") {}
+#else
+		public Context() : base("ReleaseConnection") { }
+#endif
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<User>().Property(u => u.Email).HasMaxLength(255);
+			modelBuilder.Entity<FavouriteOffice>().Property(u => u.Email).HasMaxLength(255);
+		}
 
 		public DbSet<User> Users { get; set; }
 		public DbSet<FavouriteOffice> FavouriteOffices { get; set; }
